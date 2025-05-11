5ee30497d4a915f6ea2b29d5ce1a38e744757848
lucasfeva <luscas.png@gmail.com>
2025-05-11 03:42:34 -0300

    feat: add models and migrations for Habit and HabitLog

---
+++ database/migrations/2025_05_11_062549_create_habit_logs_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\Habit;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('habit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->foreignIdFor(Habit::class)->constrained();
+            $table->dateTime('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('habit_logs');
+    }
+};
